549ddf5c78e87bfba384d197cead577b2902b88a
Danang <danangmassandy@gmail.com>
2024-08-16 15:09:14 +0700

    removed unused defaults

--- cplus_core/utils/conf.py
+++ cplus_core/utils/conf.py
@@ -9,30 +9,6 @@ import enum
 class Settings(enum.Enum):
     """Plugin settings names"""
 
-    DOWNLOAD_FOLDER = "download_folder"
-    REFRESH_FREQUENCY = "refresh/period"
-    REFRESH_FREQUENCY_UNIT = "refresh/unit"
-    REFRESH_LAST_UPDATE = "refresh/last_update"
-    REFRESH_STATE = "refresh/state"
-
-    # Report settings
-    REPORT_ORGANIZATION = "report/organization"
-    REPORT_CONTACT_EMAIL = "report/email"
-    REPORT_WEBSITE = "report/website"
-    REPORT_CUSTOM_LOGO = "report/custom_logo"
-    REPORT_CPLUS_LOGO = "report/cplus_logo"
-    REPORT_CI_LOGO = "report/ci_logo"
-    REPORT_LOGO_DIR = "report/logo_dir"
-    REPORT_FOOTER = "report/footer"
-    REPORT_DISCLAIMER = "report/disclaimer"
-    REPORT_LICENSE = "report/license"
-    REPORT_STAKEHOLDERS = "report/stakeholders"
-    REPORT_CULTURE_POLICIES = "report/culture_policies"
-
-    # Last selected data directory
-    LAST_DATA_DIR = "last_data_dir"
-    LAST_MASK_DIR = "last_mask_dir"
-
     # Advanced settings
     BASE_DIR = "advanced/base_dir"
 
@@ -69,11 +45,3 @@ class Settings(enum.Enum):
     LANDUSE_NORMALIZED = "landuse_normalized"
     LANDUSE_WEIGHTED = "landuse_weighted"
     HIGHEST_POSITION = "highest_position"
-
-    # Processing option
-    PROCESSING_TYPE = "processing_type"
-
-    # DEBUG
-    DEBUG = "debug"
-    DEV_MODE = "dev_mode"
-    BASE_API_URL = "base_api_url"
