02e789a83c50ed8d1c7e6f7a4a48424750a54d80
Robert Ohuru <robertohuru@gmail.com>
2025-06-20 00:08:41 +0200

    Add unit tests

---
+++ tests/test_utils.py
@@ -0,0 +1,39 @@
+# coding=utf-8
+"""Tests for the CPLUS plugin utilities.
+
+"""
+import unittest
+
+from cplus_core.utils.helper import (
+    align_rasters,
+    todict,
+    transform_extent,
+    normalize_raster_layer,
+    replace_nodata_value_from_reference
+)
+
+
+class CplusCoreUtilTest(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_transform_extent(self):
+        """Test the transform_extent function."""        
+        self.assertFalse(False)
+
+    def test_align_rasters(self):
+        """Test the align_rasters function."""
+        self.assertFalse(False)
+    
+    def test_todict(self):
+        """Test the todict function."""
+        self.assertFalse(False)
+
+    def test_normalize_raster_layer(self):
+        """Test the normalize_raster_layer function."""
+        self.assertFalse(False)
+    
+    def test_replace_nodata_value_from_reference(self):
+        """Test the replace_nodata_value_from_reference function."""
+        self.assertFalse(False)
+    
