8081f2b01b089ad4f1c19880dece6669570909cf
Robert Ohuru <robertohuru@gmail.com>
2025-11-28 02:04:53 +0100

    Rewrite pixel connectity unit tests

--- tests/test_scenario_tasks.py
+++ tests/test_scenario_tasks.py
@@ -672,8 +672,8 @@ class ScenarioAnalysisTaskTest(unittest.TestCase):
         result_layer = QgsRasterLayer(test_activity.path, test_activity.name)
 
         result_stat = result_layer.dataProvider().bandStatistics(1)
-        self.assertAlmostEqual(result_stat.minimumValue, 1.11)
-        self.assertAlmostEqual(result_stat.maximumValue, 1.86)
+        self.assertAlmostEqual(result_stat.minimumValue, 0.825, places=3)
+        self.assertAlmostEqual(result_stat.maximumValue, 1.825, places=3)
 
         self.assertTrue(result_layer.isValid())
 
